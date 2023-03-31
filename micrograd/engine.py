c13cb44e0a1308d5500922647411359dced2bf1e
Nathan24zz <nathanterbaik@gmail.com>
2023-03-31 14:48:36 +0700

    feat: add var to trace prev value and operation

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,14 +1,17 @@
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0
+        self._prev = set(_children)
+        self._op = _op  # the op that produced this node, for graphviz / debugging / etc
+        self.label = label  # for testing purpose
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other), '+')
         return out
 
     def __radd__(self, other):  # other + self
@@ -18,7 +21,7 @@ class Value:
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other), '*')
         return out
 
     def __rmul__(self, other):  # other * self
