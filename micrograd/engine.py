4393d713c348502774a873bd20b9da079e381bb8
Nathan24zz <nathanterbaik@gmail.com>
2023-04-01 22:51:57 +0700

    refactor: breaking up tanh

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -39,13 +39,21 @@ class Value:
             self.grad += 1.0 * out.grad
             other.grad += 1.0 * out.grad
         out._backward = _backward
-
         return out
 
     def __radd__(self, other):  # other + self
         # reverse add operation
         # ex: int + Value -> Value + int
         return self + other
+
+    def __neg__(self):  # -self
+        return self * -1
+
+    def __sub__(self, other):  # self - other
+        return self + (-other)
+
+    def __rsub__(self, other):  # other - self
+        return other + (-self)
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -55,11 +63,25 @@ class Value:
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
         out._backward = _backward
-
         return out
 
     def __rmul__(self, other):  # other * self
         return self * other
+
+    def __truediv__(self, other):  # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other):  # other / self
+        return other * self**-1
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data ** other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        out._backward = _backward
+        return out
 
     def tanh(self):
         x = self.data
@@ -68,5 +90,13 @@ class Value:
 
         def _backward():
             self.grad += 1 - out.data**2
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        out = Value(math.exp(self.data), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
         out._backward = _backward
         return out
